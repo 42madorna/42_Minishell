bc26e56c16f2b0007893399fd5ea538a13f9e9a3
Miguel angel Adorna ruiz <madorna-@c1r11s5.42madrid.com>
2021-11-23 01:12:15 +0100

    Added libft

---
+++ libft/ft_atol.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: madorna- <madorna-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/22 16:39:02 by madorna-          #+#    #+#             */
+/*   Updated: 2021/11/23 01:03:13 by madorna-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+long
+	ft_atol(const char *str)
+{
+	int			i;
+	int			negative;
+	long		nbr;
+
+	i = 0;
+	negative = 1;
+	nbr = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+			|| str[i] == '\r' || str[i] == '\v' || str[i] == '\f')
+		i++;
+	if (str[i] == '-')
+		negative = -1;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i] <= '9' && str[i] >= '0')
+	{
+		nbr = nbr * 10 + str[i] - '0';
+		i++;
+	}
+	return (nbr * negative);
+}
