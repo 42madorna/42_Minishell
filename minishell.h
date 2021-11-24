8f0a97bea9d862ce60abf5817d6246785390aaa1
Miguel angel Adorna ruiz <madorna-@c1r15s4.42madrid.com>
2021-11-24 02:50:53 +0100

    MINS-50 (MINS-51, MINS-52, MINS-54) done some parsing

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: madorna- <madorna-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 00:53:12 by madorna-          #+#    #+#             */
-/*   Updated: 2021/11/24 01:07:27 by madorna-         ###   ########.fr       */
+/*   Updated: 2021/11/24 02:47:15 by madorna-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,7 @@ typedef struct	s_cmd
 	int		argc;
 	char	**argv;
 	char	**env;
+	t_list	l_argv;
 	char	*infile;
 	char	*outfile;
 }				t_cmd;
@@ -53,7 +54,7 @@ typedef struct	s_cmd
 /*
 ** Parser functions
 */
-void	parse(t_mini *mini);
+int		parse(t_mini *mini);
 
 /*
 ** Exec functions
--- parser/parse.c
+++ parser/parse.c
@@ -6,31 +6,67 @@
 /*   By: madorna- <madorna-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 00:43:51 by madorna-          #+#    #+#             */
-/*   Updated: 2021/11/24 00:51:47 by madorna-         ###   ########.fr       */
+/*   Updated: 2021/11/24 02:48:18 by madorna-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int
-	specials(char *line)
+char
+	*ft_dollar(char **line)
 {
-	if (*line == PIPE)
-		return (*line);
-	if (*line == DQUOTE)
-		return (*line);
-	if (*line == QUOTE)
-		return (*line);
-	if (*line == DOLLAR)
-		return (*line);
-	if (*line == IN)
-		return (*line);
-	if (*line == OUT)
-		return (*line);
+	char	*parsed;
+	int		i;
+
+	parsed = calloc(1, sizeof(char *));
+	i = 0;
+	++*line;
+	while (**line && **line != DQUOTE)
+	{
+		parsed[i] = **line;
+		++*line;
+		++i;
+	}
+	return (getenv(parsed));
+}
+
+int
+	find_quote(char **line, char q)
+{
+	++*line;
+	// printf("current: %s\n", *line);
+	while (*(*line))
+	{
+		// printf("current: %s\n", (*line));
+		if (q == DQUOTE && **line == '$')
+			printf("%s\n", ft_dollar(line));
+		if (**line == q)
+			return (0);
+		++*line;
+	}
+	return (1);
+}
+
+int
+	specials(char **line)
+{
+	// printf("received %s\n", *line);
+	if (*(*line) == PIPE)
+		return (0);
+	if (*(*line) == DQUOTE)
+		return (find_quote(line, DQUOTE));
+	if (*(*line) == QUOTE)
+		return (find_quote(line, QUOTE));
+	if (*(*line) == DOLLAR)
+		printf("%s\n", ft_dollar(line));
+	if (*(*line) == IN)
+		return (0);
+	if (*(*line) == OUT)
+		return (0);
 	return (0);
 }
 
-void
+int
 	parse(t_mini *mini)
 {
 	char	*line;
@@ -38,11 +74,11 @@ void
 
 	line = mini->line;
 	ft_bzero(&cmd, sizeof(t_cmd));
-	cmd.argv = ft_split(line, ' ');
+	cmd.env = mini->env;
 	while (*line)
 	{
-		if (specials(line) != 0)
-			printf("Special char detected\n");
+		if (specials(&line))
+			return (printf("Unexpected token\n"));
 		line++;
 	}
 	ft_execve(cmd);
@@ -57,4 +93,5 @@ void
 	// 	write(1, line, 1);
 	// 	++line;
 	// }
+	return (0);
 }
