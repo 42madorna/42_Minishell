7248cfe7d80b6f87064989e33db5932969a84fa3
Miguel Ángel Adorna Ruiz (madorna-) <madorna-@student.42madrid.com>
2021-12-01 01:51:59 +0100

    MINS-63 fixed not adding NULL to last argv

--- parser/make_argv.c
+++ parser/make_argv.c
@@ -6,7 +6,7 @@
 /*   By: madorna- <madorna-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 06:35:25 by madorna-          #+#    #+#             */
-/*   Updated: 2021/11/30 20:27:46 by madorna-         ###   ########.fr       */
+/*   Updated: 2021/12/01 01:46:09 by madorna-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,12 @@ void
 
 	l_argv = cmd->l_argv;
 	n_argv = ft_lstsize(l_argv);
-	cmd->argv = calloc(n_argv, sizeof(char*));
+	cmd->argv = calloc(n_argv + 1, sizeof(char*));
 	i = 0;
 	while (l_argv)
 	{
-		cmd->argv[i] = l_argv->content;
+		cmd->argv[i++] = l_argv->content;
 		l_argv = l_argv->next;
-		i++;
 	}
 }
 
@@ -35,7 +34,6 @@ void
 	make_argv(t_mini *mini)
 {
 	t_list	*cmds;
-	t_cmd	*cmd;
 
 	cmds = mini->cmds;
 	while (cmds)
