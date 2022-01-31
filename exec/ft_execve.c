b39a4a1a5f99bc06b2e1a0e6c3be12ed2a1585b7
Miguel Angel Adorna Ruiz <madorna-@student.42madrid.com>
2022-01-31 01:03:01 +0100

    Preparing for ft_search_command structure

---
+++ exec/ft_execve.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_execve.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adorna_r <adorna_r@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/24 00:25:30 by madorna-          #+#    #+#             */
+/*   Updated: 2022/01/31 00:44:14 by adorna_r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int
+	ft_execve(t_cmd command)
+{
+	pid_t	pid;
+	int		status;
+
+	// command.argv = malloc(sizeof(char*) * 2);
+	// command.argv[0] = "/usr/bin/man";
+	// command.argv[1] = "man";
+	// TODO: search in the PATH env variable for a match
+	/*
+	** if (!command)
+	**		return (printf(command not found...))
+	*/
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execve(command.argv[0], command.argv, command.env) != 0)
+			perror(strerror(errno));
+	}
+	else if (pid > 0)
+		pid = wait(&status);
+	status = WEXITSTATUS(status);
+	return (status);
+}
