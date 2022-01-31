5081721103c7909c868e40aa539f8af403249e67
Miguel angel Adorna ruiz <madorna-@c2r15s3.42madrid.com>
2022-01-31 18:07:04 +0100

    Norminized

--- builtin/ft_env.c
+++ builtin/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: madorna- <madorna-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/07 05:13:01 by madorna-          #+#    #+#             */
-/*   Updated: 2022/01/31 18:03:25 by madorna-         ###   ########.fr       */
+/*   Updated: 2022/01/31 18:06:43 by madorna-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,10 @@ void
 	t_list	*env_node;
 
 	env_node = l_env;
-	while(env_node)
+	while (env_node)
 	{
-		printf("%s=%s\n", ((t_env*)(env_node->content))->key, ((t_env*)(env_node->content))->value);
+		printf("%s=%s\n", ((t_env *)(env_node->content))->key,
+			((t_env *)(env_node->content))->value);
 		env_node = env_node->next;
 	}
 }
