b48f83e03bb9ec01b0d792e70e42c0b15e9e362e
Miguel Angel Adorna Ruiz <madorna-@student.42madrid.com>
2022-02-05 01:38:47 +0100

    Approach to unset

--- builtin/ft_unset.c
+++ builtin/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: madorna- <madorna-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/11 04:06:40 by madorna-          #+#    #+#             */
-/*   Updated: 2022/02/01 18:54:52 by madorna-         ###   ########.fr       */
+/*   Updated: 2022/02/05 01:37:25 by madorna-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,33 +23,51 @@
 ** Unset takes just the first argument, ignores the rest.
 */
 
+void
+	ft_del_env_node(void *env)
+{
+	t_env	*env_node;
+
+	env_node = env;
+	if (env_node->key)
+	{
+		free(env_node->key);
+		env_node->key = NULL;
+	}
+	if (env_node->value)
+	{
+		free(env_node->value);
+		env_node->value = NULL;
+	}
+}
 /*
 ** TODO: Remove env variable
 */
-// void
-// 	del_env_vble(t_list *l_env, char *str)
-// {
-// 	t_list	*env_node;
-// 	t_list	*del;
+void
+	del_env_vble(t_list *l_env, char *str)
+{
+	t_list	*env_node;
+	t_list	*del;
 
-// 	env_node = l_env;
-// 	while (env_node)
-// 	{
-// 		if (ft_strncmp(((t_env *)env_node->content)->key, str, ft_strlen(((t_env *)env_node->content)->key)))
-// 		{
-// 			del = env_node;
-// 			break ;
-// 		}
-// 		env_node = env_node->next;
-// 	}
-// 	while (env_node && env_node->next != del)
-// }
+	env_node = l_env;
+	while (env_node)
+	{
+		if (!ft_strncmp(((t_env *)env_node->content)->key, str, ft_strlen(((t_env *)env_node->content)->key)))
+		{
+			del = env_node;
+			break ;
+		}
+		env_node = env_node->next;
+	}
+	ft_lstdelone(del, ft_del_env_node);
+}
 
 int
 	ft_unset(int argc, char **argv, t_list *l_env)
 {
-	write(1, "\n", 1);
+	// write(1, "\n", 1);
 	// if (argc >= 2)
-	// 	del_env_vble(env, argv[1]);
+	// 	del_env_vble(l_env, argv[1]);
+	printf("TODO: [MINS-77] unset builtin\n");
 	return (0);
 }
