03634279df2cf621028ce49d23f653ff4db92ac5
Miguel Angel Adorna Ruiz <madorna-@student.42madrid.com>
2022-02-05 01:01:45 +0100

    Minishell now fills necessary env even if unexistent. MINS-86 FIXED

--- env/ft_env_to_lst.c
+++ env/ft_env_to_lst.c
@@ -6,11 +6,56 @@
 /*   By: madorna- <madorna-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 01:38:49 by madorna-          #+#    #+#             */
-/*   Updated: 2022/01/31 04:48:24 by madorna-         ###   ########.fr       */
+/*   Updated: 2022/02/05 00:57:59 by madorna-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static inline int
+	ft_has_env_var(t_mini *mini, char *str)
+{
+	t_list	*env_node;
+	t_env	*env_cont;
+
+	env_node = mini->l_env;
+	while (env_node)
+	{
+		env_cont = env_node->content;
+		if (!ft_strncmp(env_cont->key, str, ft_strlen(str) + 1))
+			return (1);
+		env_node = env_node->next;
+	}
+	return (0);
+}
+
+static inline void
+	ft_add_basic_env(t_mini *mini)
+{
+	t_env	*env_node;
+
+	if (!ft_has_env_var(mini, "OLDPWD"))
+	{
+		env_node = calloc(1, sizeof(t_env));
+		env_node->key = ft_strdup("OLDPWD");
+		env_node->value = NULL;
+		ft_lstadd_back(&mini->l_env, ft_lstnew(env_node));
+	}
+	if (!ft_has_env_var(mini, "PWD"))
+	{
+		env_node = calloc(1, sizeof(t_env));
+		env_node->key = ft_strdup("PWD");
+		env_node->value = getcwd(env_node->value, 0);
+		ft_lstadd_back(&mini->l_env, ft_lstnew(env_node));
+	}
+	if (!ft_has_env_var(mini, "SHLVL"))
+	{
+		env_node = calloc(1, sizeof(t_env));
+		env_node->key = ft_strdup("SHLVL");
+		env_node->value = ft_strdup("1");
+		ft_lstadd_back(&mini->l_env, ft_lstnew(env_node));
+	}
+}
 
 void
 	ft_env_to_lst(t_mini *mini)
@@ -32,4 +77,5 @@ void
 		// Uncoment this to see each env var key:value
 		// printf("key %s, value %s\n", env_node->key, env_node->value);
 	}
+	ft_add_basic_env(mini);
 }
