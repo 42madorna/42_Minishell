1aa16d2be67361d5a73175ae61a9eba5582f61d7
somedevv <adriancrmini@gmail.com>
2022-03-01 16:38:23 +0100

    MINS-111  Leaks: " and ' fixed

--- parser/quote_finder.c
+++ parser/quote_finder.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   quote_finder.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: madorna- <madorna-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 18:07:44 by madorna-          #+#    #+#             */
-/*   Updated: 2022/02/14 00:33:05 by madorna-         ###   ########.fr       */
+/*   Updated: 2022/03/01 16:34:13 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,6 +62,7 @@ void
 	if (unclosed)
 	{
 		printf("Unexpected token near `%s'\n", unclosed_name(unclosed));
+		ft_lstdelone(mini->chars, free);
 		mini->parse_err = 1;
 	}
 }
